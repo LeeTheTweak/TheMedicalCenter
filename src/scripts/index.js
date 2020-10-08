8ae462b038739d9596ae28cfd0d1531dac58255a
LeeTheTweak <chrisleveston@gmail.com>
2020-10-07 23:19:35 -0500

    Added the Donny chatbox along with the styling with text and a textbox inside of the donny chatbox.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,33 @@
 
 $(() => {
 
+    // Getting the chat in the chatbox.
+
+    let chat = document.getElementsByClassName("chat")[0];
+
+    // Create a donny chat element.
+
+    let donnyInitChatEl = document.createElement("P");
+
+    // Append an initial string to the donnyInitChatEl.
+
+    let donnyInitMessage = "Greetings. How may I help you?";
+
+    // Add donnyInitMessage to the donnyInitChatEl.
+
+    donnyInitChatEl.textContent = donnyInitMessage;
+
+    // Append the created donny element to the chat.
+
+    chat.append(donnyInitChatEl);
+
 
     // When Donny image is clicked, display donny chat box.
 
     $(".virtual-assistant-image").on("click", () => {
         $(".donny-chatbox").css({ 
-            "display" : "block" 
+            "opacity" : "1.0" ,
+            "transition" : "all .1s ease-in-out"
         });
     })
 
@@ -14,7 +35,8 @@ $(() => {
 
     $(".fa-times").on("click", () => {
         $(".donny-chatbox").css({ 
-            "display" : "none" 
+            "opacity" : "0.0",
+            "transition" : "all .1s ease-in-out"
         });
     });
 
